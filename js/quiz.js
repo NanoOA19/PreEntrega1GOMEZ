23eae899882572e6c1918c26c606ad67cc6b5b50
nano <enzo.fg117@gmail.com>
2024-01-17 23:22:45 -0300

    finalizacion de primer pre entrega

--- js/quiz.js
+++ js/quiz.js
@@ -1,31 +1,65 @@
 alert("HOLA PADELISTA")
-let entrada = prompt("Ingrese su nombre");
-let salida = entrada + " " + "es su nombre";
-alert(salida);
-let unColor =prompt("Escriba su color favorito");  
-if (unColor =="rojo"){
-    alert("Sos un capo man");
-}else{
-    alert("Aguante el rojo gil");
-
-}
-let unColor2 =prompt("Escriba su color favorito");  
-if (unColor2 ==""){
-    alert("no ingresaste color");
-}else{
-    alert("Color ingresado " + unColor2);
-
-}
-let nombre = prompt("Ingresar Nombre");
-let apellido = prompt("Ingresar Apellido");
-
-if((nombre !="") && (apellido !="")){
-    alert("Nombre: "+ nombre + "\nApellido: "+ apellido);
-}else{
-    alert("ERROR: NOMBRE Y APELLIDO BOLUDO");
-}
-let tabla = parseInt(prompt("Ingresar numero para saber su tabla"));
-for( let i = 1; i <= 10; i++){
-    let resultado = tabla * i;
-    alert(tabla +" X " + i + " = " + resultado);
-}
+    let nombreUsuario= prompt("ingrese su nombre");
+    let gustaUsuario= prompt("¿Te gusta el padel?");
+    let totalRestpuestas=0
+    gustaUsuario= gustaUsuario.toUpperCase()
+    
+    
+        if(gustaUsuario == "SI")  {
+            
+            let empiezaEljuego= confirm("Quieres jugar un juego? "+nombreUsuario);
+            while (empiezaEljuego == true){
+                let nPregunta=prompt("Elija un numero entre el 1 y el 3 inclusive");
+                switch(nPregunta){
+                case "1":
+                    let respuesta1= prompt("¿Quien fue el ultimo campeon del WPT?");
+                        respuesta1=respuesta1.toLocaleUpperCase()
+                        if(respuesta1 == "ROBERT MIZRACHI"){
+                            totalRestpuestas += 1
+                            alert("Bien pa, +1 punto. Vas: " + totalRestpuestas)
+                        }
+                        else{
+                            totalRestpuestas -=1
+                            alert(" La pifiaste brother, es ROBERT MIZRACHI \n -1 punto Vas: " + totalRestpuestas)
+                        }
+                    break;
+                case "2":
+                    let respuesta2= prompt("¿Cuando es el proximo WPT?")
+                        respuesta2=respuesta2.toLocaleUpperCase
+                        if(respuesta2 == "23 DE MAYO"){
+                            totalRestpuestas +=1
+                            alert("Bien pa, +1 punto. Vas:" + totalRestpuestas)
+                        }
+                        else{
+                            totalRestpuestas -=1
+                            alert(" La pifiaste brother, es el 23 DE MAYO \n -1 punto:" + totalRestpuestas)
+                        }
+                    break;
+                case "3":
+                    let respuesta3= prompt("¿Quienes quedaron segundos en el ultimo WPT?")
+                    respuesta3=respuesta3.toLocaleUpperCase()
+                        if(respuesta3 == "LEBRON Y GALAN"){
+                            totalRestpuestas +=1
+                           alert("Bien pa, +1 punto. Vas:" + totalRestpuestas)
+                        }
+                        else{
+                            totalRestpuestas -=1
+                            alert("La pifiaste brother, son LEBRON Y GALAN \n -1 punto:" + totalRestpuestas)
+                        }
+                    break;
+                default:
+                     alert("Dije numero, no letra >:(")
+                     alert("Puntos totales:"+ totalRestpuestas)
+                     empiezaEljuego= confirm("Quieres seguir jugando?")
+                    break;
+                }
+            }
+            if(empiezaEljuego == false){
+                alert("Pense que eras chevere")
+            }
+    
+            
+        }
+        else{
+            alert("Muuy mal no eres digno de participar")
+        }
